b748a0199ce7ae50c56145da7d2284ed722cc13a
Guna-meda <meda.gunavathi25@gmail.com>
2025-08-04 22:13:18 +0530

    albums

--- src/routes/album.route.js
+++ src/routes/album.route.js
@@ -4,6 +4,11 @@ import { getAlbumById, getAllAlbums } from "../controller/album.controller.js";
 const router= Router();
 
 router.get ("/" ,getAllAlbums );
-router.get("/albumId",getAlbumById)
+//router.get("/:albumId",getAlbumById)
+router.get("/:albumId", (req, res, next) => {
+  console.log("HIT ALBUM ROUTE with ID:", req.params.albumId);
+  next(); // pass to controller
+}, getAlbumById);
+
 
 export default router;
